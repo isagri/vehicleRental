7d633b83e73d211a57113267b57fbec8824f4056
isagri <isabelle.grillet6@orange.fr>
2018-09-04 17:06:13 +0200

    affiche n°réservation

--- src/com/campusnumerique/vehicle/servlet/confirm/ConfirmServlet.java
+++ src/com/campusnumerique/vehicle/servlet/confirm/ConfirmServlet.java
@@ -29,7 +29,7 @@ public class ConfirmServlet extends HttpServlet {
     private ClientDAO clientDAO=null;
     private CarDAO carDAO=null;
     private BookingDAO bookingDAO=null;
-	public static final String VUE = "/pages/homePage.jsp";
+	public static final String VUE = "/pages/confirm.jsp";
 	public static final String VUEPOST = "/pages/booking.jsp";
 
 	
@@ -82,7 +82,7 @@ public class ConfirmServlet extends HttpServlet {
 			e.printStackTrace();
 		}
 		
-	
+		request.setAttribute("booking", booking);
     	this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
     
--- src/com/campusnumerique/vehiclerental/dao/BookingDAO.java
+++ src/com/campusnumerique/vehiclerental/dao/BookingDAO.java
@@ -4,12 +4,14 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.util.List;
 
 import com.campusnumerique.vehiclerental.entity.Booking;
 import com.campusnumerique.vehiclerental.entity.Client;
+import com.mysql.cj.protocol.Resultset;
 
 public class BookingDAO extends DAO<Booking>{
 
@@ -57,7 +59,7 @@ public class BookingDAO extends DAO<Booking>{
 		
 		String sql = "INSERT INTO booking (id_client, id_car, startDate, endDate, estimatedDistance, estimatedPrice) VALUES (?,?,?,?,?,?)";
 		
-		preparedStatement = connection.prepareStatement(sql);
+		preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		
 
 		preparedStatement.setInt(1, booking.getClient().getId());
@@ -70,7 +72,11 @@ public class BookingDAO extends DAO<Booking>{
 		
 		preparedStatement.executeUpdate();
 		
-		
+		ResultSet rs = preparedStatement.getGeneratedKeys();
+		if (rs.first()) {
+			booking.setId(rs.getInt(1));
+		}
+			
 	}
 
 	@Override
